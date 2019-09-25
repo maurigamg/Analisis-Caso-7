16d3717958967b9cfa58124de24de20b40fd825f
maurigamg <44620109+maurigamg@users.noreply.github.com>
2019-09-25 11:25:01 -0600

    Actualización rama master
    Agregué el proyecto Caso 7, el cual es la base de la rama de desarrollo

---
+++ Caso 7/src/aplicacion/Prueba.java
@@ -0,0 +1,12 @@
+package aplicacion;
+
+/**
+ * 
+ * @author Mauricio Gamboa
+ * @author Jose Macias
+ * @version 21/09/2019
+ */
+public class Prueba {
+  public static void main(String[] args) {
+  }
+}
