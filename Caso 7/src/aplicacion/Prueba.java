f5afff615340a73ff18b3393ecc0f4f5a0855e8c
maurigamg <44620109+maurigamg@users.noreply.github.com>
2019-09-25 17:36:43 -0600

    Actualización feature_mauricio #2
    Decidí que era mejor que los métodos randomizeLetras y randomizeNumeros fueran privados, coloque el método de realizarPrueba, el cual va a ser el que devuelva el mensaje desencriptado más adelante junto a otros datos.

--- Caso 7/src/aplicacion/Prueba.java
+++ Caso 7/src/aplicacion/Prueba.java
@@ -13,7 +13,7 @@ public class Prueba {
 	Probabilidad probabilidad;
 	probabilidad = new Probabilidad("xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmy"
 	    + "N6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=");
-	probabilidad.randomizeNumeros();
-	probabilidad.randomizeLetras();
+
+	System.out.println(probabilidad.realizarPrueba());
   }
 }
--- Caso 7/src/logica/Probabilidad.java
+++ Caso 7/src/logica/Probabilidad.java
@@ -10,23 +10,25 @@ public class Probabilidad {
   private char letras[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
       's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
   private char numeros[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-  private final String passwordPart1 = "29dh120";
-  private final String passwordPart2 = "dk1";
-  private final String passwordPart3 = "3";
+  private final String keyPart1 = "29dh120";
+  private final String keyPart2 = "dk1";
+  private final String keyPart3 = "3";
   private String encriptado;
+  private int intentos = 0;
 
+  /**
+   * Constructor de la clase Probabilidad
+   * 
+   * @param encriptado Corresponde al mensaje encriptado
+   */
   public Probabilidad(String encriptado) {
-	this.encriptado = encriptado;
-  }
-
-  public void setEncriptado(String encriptado) {
 	this.encriptado = encriptado;
   }
 
   /**
    * Metodo para tener un orden random de letras
    */
-  public void randomizeLetras() {
+  private void randomizeLetras() {
 	int maximo = letras.length;
 	int cambio;
 	for (int actual = 0; actual < maximo; actual++) {
@@ -40,7 +42,7 @@ public class Probabilidad {
   /**
    * Metodo para tener un orden random de numeros
    */
-  public void randomizeNumeros() {
+  private void randomizeNumeros() {
 	int maximo = numeros.length;
 	int cambio;
 	for (int actual = 0; actual < maximo; actual++) {
@@ -49,5 +51,16 @@ public class Probabilidad {
 	  numeros[cambio] = numeros[actual];
 	  numeros[actual] = temporal;
 	}
+  }
+
+  /**
+   * Metodo que realiza las pruebas hasta obtener una combinacion correcta para descifrar
+   * 
+   * @return Un String con los intentos que realizo, el mensaje descifrado y la llave utilizada
+   */
+  public String realizarPrueba() {
+	this.randomizeLetras();
+	this.randomizeNumeros();
+	return null;
   }
 }
