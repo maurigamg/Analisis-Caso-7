86cf9dabe01f9c63bebfc6111d5b7cbff45800a6
daniel-macias <josedanielmr@gmail.com>
2019-09-27 03:23:09 -0600

    ya funciona lo de subir posibilidades para futuros intentos

--- Caso 7/src/aplicacion/Prueba.java
+++ Caso 7/src/aplicacion/Prueba.java
@@ -1,5 +1,7 @@
 package aplicacion;
 
+import java.util.Scanner;
+
 import logica.Probabilidad;
 
 /**
@@ -16,6 +18,23 @@ public class Prueba {
 	    + "N6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=");
 	
 	probabilidad.crearConjuntos();
-	System.out.println(probabilidad.realizarPrueba());
+	probabilidad.realizarPrueba();
+	
+	Scanner scanner = new Scanner(System.in);
+    String readString = scanner.nextLine();
+    while(readString!=null) {
+        System.out.println(readString);
+        probabilidad.realizarPrueba();
+        if (readString.isEmpty()) {
+            System.out.println("Gracias por usar el programa");
+            break;
+        }
+
+        if (scanner.hasNextLine()) {
+            readString = scanner.nextLine();
+        } else {
+            readString = null;
+        }
+    }
   }
 }
